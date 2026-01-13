85f4f24f120a38ef8fcc0e926b77c73700916f78
SamanthaLu <samanthat511ss@gmail.com>
2026-01-13 17:43:21 +0800

    finish BMI counting

--- final_dataset.R
+++ final_dataset.R
@@ -1,5 +1,11 @@
-#select column from different dataset----
+#Select column from different dataset----
 ##exposure selection----
 exposure<-exposureb45y_clean%>%
   select(Sampleid,probioticintake)
 ##covariate selection----
+TBCSstimu5y<-TBCSstimu%>%
+  select(Sampleid,B_SEX,height_5y,weight_5y,dairyintake_5y,fedu_5y,medu_5y)
+###BMI counting----
+TBCSstimu5y<-TBCSstimu5y%>%
+  mutate(BMI_5y=weight_5y/(height_5y/100)^2)
+##outcome selection----
