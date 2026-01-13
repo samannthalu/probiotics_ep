3a39bfba8f738dd198b95de78ea7c7738280d28f
SamanthaLu <samanthat511ss@gmail.com>
2026-01-13 19:42:04 +0800

    finish final dataset

--- final_dataset.R
+++ final_dataset.R
@@ -9,3 +9,14 @@ TBCSstimu5y<-TBCSstimu%>%
 TBCSstimu5y<-TBCSstimu5y%>%
   mutate(BMI_5y=weight_5y/(height_5y/100)^2)
 ##outcome selection----
+IPD_outcome<-NHIRD_IPD%>%
+  select(Sampleid,IN_DATE,EarlyPuberty_IPD)
+OPD_outcome<-NHIRD_OPD%>%
+  select(Sampleid,FUNC_DATE,EarlyPuberty_OPD)
+###combine NHIRD OPD/IPD outcome(is this neccesary?)
+outcome
+#Combine final dataset
+TBCSstimu_fixed<-TBCSstimu5y%>%
+  full_join(exposure,by="Sampleid")%>%
+  full_join(NHIRD_IPD,by="Sampleid")%>%
+  full_join(NHIRD_OPD,by="Sampleid")
