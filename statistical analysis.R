70799da0c4bdcbeaa6514ae764f84a6d6b179678
SamanthaLu <samanthat511ss@gmail.com>
2026-06-08 19:23:50 +0800

    fix label categories and add table 1 (no BMI ver.)

--- statistical analysis.R
+++ statistical analysis.R
@@ -14,31 +14,32 @@ dat1$breastfeeding<-factor(dat1$breastfeeding,
                            labels = c("No breastfeeding","breastfeeding"))
 dat1$dairyintake_5y<-factor(dat1$dairyintake_5y,
                             levels = c(0,1,2,3,4,5,8,9),
-                            labels = c("Never",
-                                     "less than 1 time a week",
+                            labels = c("Never or less than 1 time a week",
+                                     "Never or less than 1 time a week",
                                      "1-2 times a week",
                                      "3 to 5 times a week",
                                      "everyday/almost everyday",
-                                     "Unsure","Not Applicable",
-                                     "Unknown"))
+                                     "everyday/almost everyday",
+                                     "everyday/almost everyday",
+                                     "everyday/almost everyday"))
 dat1$medu_5y<-factor(dat1$medu_5y,
                      levels=c(1,2,3),
                      labels=c("Junior High&below",
                               "Senior High/Vocational",
                               "University&above"))
 dat1$Socioeco_5y<-factor(dat1$Socioeco_5y,
-                         levels=c(1,2,3,4,5,6,7,8,9,88,98,99),
-                         labels=c("<10,000",
-                                  ">=10,000,<20,000",
-                                  ">=20,000,<30,000",
+                         levels=c(01,02,03,04,05,06,07,08,09,88,98,99),
+                         labels=c("30,000",
+                                  "30,000",
+                                  "30,000",
                                   ">=30,000,<50,000",
                                   ">=50,000,<70,000",
                                   ">=70,000,<100,000",
                                   ">=100,000,<150,000",
                                   ">=150,000,<200,000",
                                   ">=200,000",
-                                  "Not Appliable",
-                                  "Refused/Don't Know",
+                                  "Unknown",
+                                  "Unknown",
                                   "Unknown"))
 dat1$BMI_5y<-as.numeric(dat1$BMI_5y)
 
@@ -65,7 +66,72 @@ dat1 %>%
   summary_factorlist(dependent, explanatory, 
                      p=TRUE, add_dependent_label=TRUE) -> t1
 write.csv(t1,"Table1_result.csv",row.names = FALSE)
+##Table 1(no BMI version)----
+dat1<-Finaldataset
+dat1$probioticintake<-factor(dat1$probioticintake,
+                             levels = c(0, 1),
+                             labels = c("Neverusers", "Everusers"))
+dat1$B_SEX<-factor(dat1$B_SEX,
+                   levels = c(1, 2),
+                   labels = c("Male", "Female"))
+dat1$EarlyPuberty<-factor(dat1$EarlyPuberty,
+                          levels = c(0,1),
+                          labels = c("No Early Puberty","Early Puberty"))
+dat1$breastfeeding<-factor(dat1$breastfeeding,
+                           levels = c(0,1),
+                           labels = c("No breastfeeding","breastfeeding"))
+dat1$dairyintake_5y<-factor(dat1$dairyintake_5y,
+                            levels = c(0,1,2,3,4,5,8,9),
+                            labels = c("Never or less than 1 time a week",
+                                       "Never or less than 1 time a week",
+                                       "1-2 times a week",
+                                       "3 to 5 times a week",
+                                       "everyday/almost everyday",
+                                       "everyday/almost everyday",
+                                       "everyday/almost everyday",
+                                       "everyday/almost everyday"))
+dat1$medu_5y<-factor(dat1$medu_5y,
+                     levels=c(1,2,3),
+                     labels=c("Junior High&below",
+                              "Senior High/Vocational",
+                              "University&above"))
+dat1$Socioeco_5y<-factor(dat1$Socioeco_5y,
+                         levels=c(01,02,03,04,05,06,07,08,09,88,98,99),
+                         labels=c("30,000",
+                                  "30,000",
+                                  "30,000",
+                                  ">=30,000,<50,000",
+                                  ">=50,000,<70,000",
+                                  ">=70,000,<100,000",
+                                  ">=100,000,<150,000",
+                                  ">=150,000,<200,000",
+                                  ">=200,000",
+                                  "Unknown",
+                                  "Unknown",
+                                  "Unknown"))
 
+label(dat1$probioticintake) <- "Probiotic intake"
+label(dat1$B_SEX)           <- "Sex"
+label(dat1$dairyintake_5y)  <- "Dairy intake at age 5"
+label(dat1$medu_5y)         <- "Maternal education level"
+label(dat1$Socioeco_5y)     <- "Average month family income(NT$)"
+label(dat1$EarlyPuberty)    <- "Early Puberty"
+label(dat1$breastfeeding)   <- "Breast feeding"
+tab1<-table1( ~ B_SEX+dairyintake_5y+medu_5y+Socioeco_5y+EarlyPuberty+breastfeeding | probioticintake, 
+              data = dat1)
+save_html(tab1, "table1.html")
+#table1 p-value
+explanatory = c("EarlyPuberty",
+                "B_SEX",
+                "dairyintake_5y",
+                "breastfeeding",
+                "medu_5y",
+                "Socioeco_5y")
+dependent = 'probioticintake'
+dat1 %>%
+  summary_factorlist(dependent, explanatory, 
+                     p=TRUE, add_dependent_label=TRUE) -> t1
+write.csv(t1,"Table1_result.csv",row.names = FALSE)
 #Table2----
 explanatory=c("probioticintake",
               "B_SEX",
