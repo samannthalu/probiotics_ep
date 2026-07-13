91f1fd87fdde80b43ea9d6c3e07508dd2ff15b0e
SamanthaLu <samanthat511ss@gmail.com>
2026-07-13 16:51:02 +0800

    add AD Variable

--- organize_variables.R
+++ organize_variables.R
@@ -22,14 +22,13 @@ sixmdata<-read_csv("/Users/samanthalu/Desktop/thesis/TBCS_stimulated_V2/TBCS_6m_
 eighteenmdata<-read_csv("/Users/samanthalu/Desktop/thesis/TBCS_stimulated_V2/TBCS_18m_simulated.csv")
 threeydata<-read_csv("/Users/samanthalu/Desktop/thesis/TBCS_stimulated_V2/TBCS_3y_simulated.csv")
 fiveydata<-read_csv("/Users/samanthalu/Desktop/thesis/TBCS_stimulated_V2/TBCS_5y_simulated.csv")
-eightydata<-read_csv("/Users/samanthalu/Desktop/thesis/TBCS_stimulated_V2/TBCS_8y_simulated.csv")
 NHIRD_IPD<-read_csv("/Users/samanthalu/Desktop/thesis/TBCS_NHIRD_stimulated/NHIRD_IPD_simulated.csv")
 NHIRD_OPD<-read_csv("/Users/samanthalu/Desktop/thesis/TBCS_NHIRD_stimulated/NHIRD_OPD_simulated.csv")
 #select and rename variables----
 ##6month----
 sixmdata_fixed<-sixmdata%>%
   select(Sampleid,B_SEX,C4a4,C2ad,C2aM,Medu,H13,
-         CVR8,CVR9,CVR10,E5a_2,E5b2_2)
+         CVR8,CVR9,CVR10,E5a_2,E5b2_2,E5a_3,E5b3_1)
 
 sixmdata_fixed<-sixmdata_fixed%>%
   rename(probioticintake_6m=C4a4,
@@ -40,32 +39,37 @@ sixmdata_fixed<-sixmdata_fixed%>%
          m6_year=CVR8,
          m6_month=CVR9,
          m6_day=CVR10,
-         physician_diagosis_6m=E5a_2,
-         gastroenteritis_6m=E5b2_2)
+         doc_gastro_6m=E5a_2,
+         gastroenteritis_6m=E5b2_2,
+         doc_AD_6m=E5a_3,
+         AD_6m=E5b3_1)
 ##18month----
 eighteenmdata_fixed<-eighteenmdata%>%
-  select(Sampleid,D4a4,D2b,Medu,F10,E3a_2,E3b2_2)
+  select(Sampleid,D4a4,D2b,Medu,F10,E3a_2,E3b2_2,E3a_3,E3b3_2)
 
 eighteenmdata_fixed<-eighteenmdata_fixed%>%
   rename(probioticintake_18m=D4a4,
          dairyintake_18m=D2b,
          medu_18m=Medu,
          Socioeco_18m=F10,
-         physician_diagnosis_18m=E3a_2,
-         gastroenteritis_18m=E3b2_2)
+         doc_gastro_18m=E3a_2,
+         gastroenteritis_18m=E3b2_2,
+         doc_AD_18m=E3a_3,
+         AD_18m=E3b3_2)
 ##3yeard-old----
 threeydata_fixed<-threeydata%>%
-  select(Sampleid,D6a4,Medu,F14,E3a2,E3b2_2)
+  select(Sampleid,D6a4,Medu,F14,E3a2,E3b2_2,E4c_6)
 
 threeydata_fixed<-threeydata_fixed%>%
   rename(probioticintake_3y=D6a4,
          medu_3y=Medu,
          Socioeco_3y=F14,
          physician_diagnosis_3y=E3a2,
-         gastroenteritis_3y=E3b2_2)
+         gastroenteritis_3y=E3b2_2,
+         doc_AD_3y=E4c_6)
 ##5year-old----
 fiveydata_fixed<-fiveydata%>%
-  select(Sampleid,D5ad,A2_2L,A2_2W,D4_8,MEDU_5Y,G13,E4a2,E4b2_b)
+  select(Sampleid,D5ad,A2_2L,A2_2W,D4_8,MEDU_5Y,G13,E4a2,E4b2_b,E5c_4)
 
 fiveydata_fixed<-fiveydata_fixed%>%
   rename(probioticintake_5y=D5ad,
@@ -75,9 +79,10 @@ fiveydata_fixed<-fiveydata_fixed%>%
          medu_5y=MEDU_5Y,
          Socioeco_5y=G13,
          physician_diagnosis_5y=E4a2,
-         gastroenteritis_5y=E4b2_b)
+         gastroenteritis_5y=E4b2_b,
+         doc_AD_5y=E5c_4)
 #join data----
 TBCSstimu<-sixmdata_fixed%>%
   full_join(eighteenmdata_fixed, by="Sampleid")%>%
   full_join(threeydata_fixed, by="Sampleid")%>%
-  full_join(fiveydata_fixed, by="Sampleid")%>%
+  full_join(fiveydata_fixed, by="Sampleid")
