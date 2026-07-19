adc043d0bfa53790e6775ef31205071b5d6b3404
SamanthaLu <samanthat511ss@gmail.com>
2026-07-20 00:32:08 +0800

    finish ph check code

--- statistical analysis.R
+++ statistical analysis.R
@@ -112,21 +112,51 @@ analytic_strict %>% finalfit(dependent, c("probioticintake", cov_A4)) -> cox_str
 write.csv(cox_strict_A4, "Cox_ep_strict_A4noBMI.csv", row.names = FALSE)
 
 #Schoenfeld PH check----
-ph_formula <- Surv(ep_followup_time, ep_event) ~ probioticintake + B_SEX +
-  dairyintake_5y + breastfeeding + medu_5y + Socioeco_5y + BMI_5y
+##PHformula+HRplot variables----
+ph_A1 <- Surv(ep_followup_time, ep_event) ~ probioticintake + B_SEX + dairyintake_5y + breastfeeding + medu_5y + Socioeco_5y + BMI_5y
+ph_A2 <- Surv(ep_followup_time, ep_event) ~ probioticintake + B_SEX + dairyintake_5y + breastfeeding + medu_5y + Socioeco_5y
+ph_A3 <- Surv(ep_followup_time, ep_event) ~ probioticintake + B_SEX + medu_5y + Socioeco_5y + BMI_5y
+ph_A4 <- Surv(ep_followup_time, ep_event) ~ probioticintake + B_SEX + medu_5y + Socioeco_5y
 
+hr_A1 <- c("probioticintake","B_SEX","dairyintake_5y","breastfeeding","medu_5y","Socioeco_5y","BMI_5y")
+hr_A2 <- c("probioticintake","B_SEX","dairyintake_5y","breastfeeding","medu_5y","Socioeco_5y")
+hr_A3 <- c("probioticintake","B_SEX","medu_5y","Socioeco_5y","BMI_5y")
+hr_A4 <- c("probioticintake","B_SEX","medu_5y","Socioeco_5y")
+
+#PH check----
+run_ph <- function(dat, ph_formula, hr_expl, tag){
+  m   <- coxph(ph_formula, data = dat, model = TRUE)
+  zph <- cox.zph(m)
+##PH p-value----
+  zt <- as.data.frame(zph$table); zt$term <- rownames(zt)
+  write.csv(zt, paste0("Schoenfeld_pvalue_ep_", tag, ".csv"), row.names = FALSE)
+  
+##PH residual plot
+  np <- nrow(zph$table) - 1                 # 變項數(扣掉 GLOBAL)
+  nc <- 3; nr <- ceiling(np / nc)           # 每列 3 格
+  png(paste0("Cox_PH_Check_ep_", tag, ".png"),
+      width = nc*4, height = nr*3.5, units = "in", res = 300)
+  par(mfrow = c(nr, nc), mar = c(4,4,2,1))  # 每格邊界留白
+  for (i in 1:np){                          # 一格一個變項,各自加參考線
+    plot(zph[i], resid = TRUE, cex = 0.7)
+    abline(h = 0, col = "red", lty = 3)
+  }
+  dev.off()
+  
+##HR forest plot----
+  png(paste0("HR_plot_ep_", tag, ".png"), width = 12, height = 10, units = "in", res = 300)
+  print(hr_plot(dat, dependent = "Surv(ep_followup_time, ep_event)",
+                explanatory = hr_expl, dependent_label = "Early puberty"))
+  dev.off()
+}
 ##loose----
-cox_ph_loose <- coxph(ph_formula, data = analytic_loose)
-zph_loose <- cox.zph(cox_ph_loose)
-zt_loose <- as.data.frame(zph_loose$table); zt_loose$term <- rownames(zt_loose)
-write.csv(zt_loose, "Schoenfeld_pvalue_ep_loose_AABMI.csv", row.names = FALSE)
-png("Schoenfeld_ep_loose_AABMI.png", width = 10, height = 8, units = "in", res = 300)
-par(mfrow = c(2,4)); plot(zph_loose); dev.off()
+run_ph(analytic_loose,  ph_A1, hr_A1, "loose_AABMI")
+run_ph(analytic_loose,  ph_A2, hr_A2, "loose_AAnoBMI")
+run_ph(analytic_loose,  ph_A3, hr_A3, "loose_A4BMI")
+run_ph(analytic_loose,  ph_A4, hr_A4, "loose_A4noBMI")
 
 ##strict----
-cox_ph_strict <- coxph(ph_formula, data = analytic_strict)
-zph_strict <- cox.zph(cox_ph_strict)
-zt_strict <- as.data.frame(zph_strict$table); zt_strict$term <- rownames(zt_strict)
-write.csv(zt_strict, "Schoenfeld_pvalue_ep_strict_AABMI.csv", row.names = FALSE)
-png("Schoenfeld_ep_strict_AABMI.png", width = 10, height = 8, units = "in", res = 300)
-par(mfrow = c(2,4)); plot(zph_strict); dev.off()
+run_ph(analytic_strict, ph_A1, hr_A1, "strict_AABMI")
+run_ph(analytic_strict, ph_A2, hr_A2, "strict_AAnoBMI")
+run_ph(analytic_strict, ph_A3, hr_A3, "strict_A4BMI")
+run_ph(analytic_strict, ph_A4, hr_A4, "strict_A4noBMI")
