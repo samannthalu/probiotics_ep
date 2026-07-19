9b27aac7f8a18d15abb522adf6adf052659466a9
SamanthaLu <samanthat511ss@gmail.com>
2026-07-20 02:29:18 +0800

    delete extra code

--- statistical analysis.R
+++ statistical analysis.R
@@ -41,7 +41,6 @@ dat1$EarlyPuberty <- factor(dat1$EarlyPuberty_loose, levels=c(0,1),
                             labels=c("No Early Puberty","Early Puberty"))
 label(dat1$EarlyPuberty) <- "Early Puberty"
 tab1 <- table1( ~ B_SEX+BMI_5y+dairyintake_5y+medu_5y+Socioeco_5y+EarlyPuberty+breastfeeding | probiotic_group, data = dat1)
-save_html(tab1, "table1_pattern_loose.html")
 explanatory = c("EarlyPuberty","B_SEX","BMI_5y","dairyintake_5y","breastfeeding","medu_5y","Socioeco_5y")
 dependent = "probiotic_group"
 dat1 %>% summary_factorlist(dependent, explanatory, p=TRUE, add_dependent_label=TRUE,
